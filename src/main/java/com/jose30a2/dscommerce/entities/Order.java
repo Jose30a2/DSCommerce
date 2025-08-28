14fe7836d19fb543dd96ef2b9089154d25dd1f16
Jose Antonio Perez <jose30a2@gmail.com>
2025-08-27 22:05:51 -0300

    equals e hashCode

--- src/main/java/com/jose30a2/dscommerce/entities/Order.java
+++ src/main/java/com/jose30a2/dscommerce/entities/Order.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -77,5 +78,18 @@ public class Order {
     // Obtener los productos que pertenecem a esta order
     public List<Product> getProducts(){
         return items.stream().map( x -> x.getProduct()).toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
